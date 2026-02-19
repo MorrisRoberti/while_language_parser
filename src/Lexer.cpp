4ce578823a42c1fd55315c7102153cc33de24db4
MorrisDesktop <morris.roberti@studioware.eu>
2026-02-19 13:00:12 +0100

    fixed some things in lexer

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -60,39 +60,60 @@ namespace WhileParser
         char c;
         while (m_file_stream.get(c) && c != std::char_traits<char>::eof())
         {
+
             word += c;
+
+            // special case for underscore identifiers that start with underscore
+            if (word[0] == '_')
+            {
+                if (isalnum(m_file_stream.peek()) || m_file_stream.peek() == '_')
+                    continue;
+
+                return Token(TokenType::IDENTIFIER, word);
+            }
 
             const auto it = m_keywords.find(word);
 
-            // matching non alphanumeric symbols
-            if (!isalnum(c))
+            // matching symbols
+            if (!isalnum(word[0]))
             {
-                if ((c == '>' || c == '<' || c == ':') && m_file_stream.peek() == '=')
-                    continue;
-
-                // special case for underscore, that is part of identifier
-                if (c == '_')
+                if ((word[0] == '>' || word[0] == '<' || word[0] == ':') && m_file_stream.peek() == '=')
                     continue;
 
                 if (it == m_keywords.end())
-                    throw std::runtime_error("The following token is not part of the language: " + word);
+                    return Token(TokenType::UNKNOWN, word);
+
+                return Token(it->second, word);
             }
 
-            if (isdigit(c))
+            // matching numbers
+            if (isdigit(word[0]) && !isalpha(m_file_stream.peek()))
             {
                 if (isdigit(m_file_stream.peek()))
                     continue;
+
                 return Token(TokenType::NUMBER, word);
             }
 
             // matching keywords
-            if (it != m_keywords.end())
-                return Token(it->second, word);
+            if (isalpha(word[0]))
+            {
+
+                if (isalnum(m_file_stream.peek()) || m_file_stream.peek() == '_')
+                    continue;
+
+                // matching keywords
+                if (it != m_keywords.end())
+                    return Token(it->second, word);
+            }
 
             // fallback to identifier
-            if (!isalnum(m_file_stream.peek()) && std::regex_match(word, std::regex("^[a-zA-Z_][a-zA-Z0-9_]*")))
+            if (!isalnum(m_file_stream.peek()) || m_file_stream.peek() != '_')
             {
-                return Token(TokenType::IDENTIFIER, word);
+                if (std::regex_match(word, std::regex("^[a-zA-Z_][a-zA-Z0-9_]*")))
+                    return Token(TokenType::IDENTIFIER, word);
+                else
+                    throw std::runtime_error("Invalid identifier: " + word);
             }
         }
 
