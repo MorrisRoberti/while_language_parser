8404fb1fbf13a3148980c65113a87918a8233003
MorrisDesktop <morris.roberti@studioware.eu>
2026-04-12 10:49:15 +0200

    added some comments to make the tests for the parser

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -5,6 +5,14 @@
 
 #include "../include/Parser.hpp"
 
+// helpers to easily create program elements without dealing with messy pointers
+std::unique_ptr<WhileParser::AssignmentNode> simpleAssignment(const std::string &variable, const std::string &simpleExpr)
+{
+    return std::move(std::make_unique<WhileParser::AssignmentNode>(variable, std::move(std::make_unique<WhileParser::ExpressionNode>(simpleExpr))));
+}
+
+// it would be better to create 3/4 templatic functions that represent various types ASTNodes, namely: UnaryNodes, BinaryNodes, If/While etc.
+
 TEST(ParserTest, BasicAssignment)
 {
     auto stream = std::make_unique<std::istringstream>("x := 10;");
@@ -14,6 +22,23 @@ TEST(ParserTest, BasicAssignment)
     auto root = parser.parse();
 
     auto other_root = std::make_unique<WhileParser::RootNode>();
+    other_root->addNode(std::move(simpleAssignment("x", "10")));
+
+    EXPECT_TRUE(root->isEqual(other_root.get()));
+}
+
+TEST(ParserTest, BasicIf)
+{
+    auto stream = std::make_unique<std::istringstream>("x := 10;");
+
+    WhileParser::Parser parser(std::move(stream));
+
+    auto root = parser.parse();
+
+    auto statement = std::move(simpleAssignment("x", "10"));
+
+    auto other_root = std::make_unique<WhileParser::RootNode>();
+
     other_root->addNode(
         std::move(std::make_unique<WhileParser::AssignmentNode>("x", std::move(std::make_unique<WhileParser::ExpressionNode>("10")))));
 
